99c8c1c7cfc678c44ab8f55efc68c950e368d807
DreMukare <dremukare@gmail.com>
2024-02-15 15:51:51 +0300

    wip: creating faq section in help page

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -1,7 +1,110 @@
+import styled from 'styled-components';
 import Layout from '../components/Layout';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const InfoHeader = styled.p`
+	font-weight: normal;
+	font-size: 1.5em;
+	text-align: center;
+	width: 70%;
+	margin: auto;
+`;
+
+const FAQSection = styled.div`
+	margin-top: 4em;
+	display: flex;
+	gap: 2em;
+`;
+
+const TabSectionContainer = styled.div``;
+const TabSection = styled.div`
+	margin-bottom: 2em;
+	display: flex;
+	flex-direction: column;
+	width: auto;
+`;
+const Tab = styled.button<{ $active: boolean }>`
+	background: none;
+	cursor: pointer;
+	border: none;
+	padding: 0.5em 4em 0.5em 1em;
+	color: ${(props) => (props.$active ? 'white' : 'inherit')};
+	background-color: ${(props) => (props.$active ? '#04DDB2' : 'white')};
+	border-radius: 5px;
+	width: 100%;
+	font-size: 1em;
+	text-align: left;
+`;
+
+const StyledLink = styled(Link)`
+	padding: 0.5em 4em 0.5em 1em;
+	text-decoration: none;
+	color: inherit;
+	cursor: pointer;
+`;
+const DropdownSectionContainer = styled.div``;
 
 const Help = () => {
-	return <Layout>Help</Layout>;
+	const [activeTab, setActiveTab] = useState('Welcome');
+
+	const FAQTags = [
+		'Welcome',
+		'Label',
+		'Train',
+		'Results',
+		'Play',
+		'Export',
+		'Improving',
+		'Tips',
+	];
+
+	const contactTags = [
+		{ title: 'Community', url: 'https://aka.ms/LobeCommunity' },
+		{ title: 'Lobe Tour', url: '/tour' },
+		{ title: 'Examples', url: '/examples' },
+		{ title: 'Contact Us', url: 'mailto:angumbao@sandtech.com' },
+	];
+
+	return (
+		<Layout>
+			<InfoHeader>
+				Everything you need to know to train great machine learning models with
+				Lobe.
+			</InfoHeader>
+			<FAQSection>
+				<TabSectionContainer>
+					<TabSection>
+						{FAQTags.map((faqTitle: string, idx: number) => (
+							<Tab
+								key={idx}
+								onClick={() => setActiveTab(faqTitle)}
+								$active={activeTab === faqTitle}
+							>
+								{faqTitle}
+							</Tab>
+						))}
+					</TabSection>
+					<TabSection>
+						{contactTags.map(
+							(
+								contact: {
+									title: string;
+									url: string;
+								},
+								idx: number
+							) => (
+								<StyledLink key={idx} to={contact.url}>
+									{contact.title}
+								</StyledLink>
+							)
+						)}
+					</TabSection>
+				</TabSectionContainer>
+				<DropdownSectionContainer></DropdownSectionContainer>
+			</FAQSection>
+		</Layout>
+	);
 };
 
 export default Help;
